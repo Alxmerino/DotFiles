12eec80dada19358f9f642d502e70dd902d513e9
Rene Merino <rmerino@amayamedia.com>
2015-10-10 12:46:37 -0400

    Added paths

---
+++ zshes/path.zsh
@@ -0,0 +1,6 @@
+# Export composer vendors
+export PATH=~/.composer/vendor/bin:$PATH
+export PATH=/Applications/MAMP/bin/php/php5.4.4/bin:$PATH #MAMP PHI setting for Composer
+export PATH=/usr/local/share/npm/bin:$PATH #Setting to use Grunt
+export PATH=~/npm/bin:$PATH #Because installed node w/o homebrew add the npm path from the local usr
+export PATH=~/.node/bin:$PATH #add path for npm install modules
