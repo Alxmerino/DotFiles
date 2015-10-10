24df7d220c41c1462c67af4ae0adbc2338466a72
Rene Merino <rmerino@amayamedia.com>
2015-10-10 13:00:49 -0400

    Added .zshrc file

--- zshes/path.zsh
+++ zshes/path.zsh
@@ -4,3 +4,9 @@ export PATH=/Applications/MAMP/bin/php/php5.4.4/bin:$PATH #MAMP PHI setting for 
 export PATH=/usr/local/share/npm/bin:$PATH #Setting to use Grunt
 export PATH=~/npm/bin:$PATH #Because installed node w/o homebrew add the npm path from the local usr
 export PATH=~/.node/bin:$PATH #add path for npm install modules
+
+# The next line updates PATH for the Google Cloud SDK.
+source $HOME/google-cloud-sdk/google-cloud-sdk/path.zsh.inc
+
+# The next line enables bash completion for gcloud.
+source $HOME/google-cloud-sdk/google-cloud-sdk/completion.zsh.inc
