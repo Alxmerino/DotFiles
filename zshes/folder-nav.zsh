5a1a3424ad09eba282cd0224b33077bfe36ad8c0
Rene Merino <rmerino@amayamedia.com>
2016-05-02 14:39:48 -0400

    Added function to load local bash file if present

--- zshes/folder-nav.zsh
+++ zshes/folder-nav.zsh
@@ -9,3 +9,19 @@ mkd() {
     mkdir -p $1
     cd $1
 }
+
+# Loads local bash
+load_local_bash(){
+    Files='./.bashrc.local';
+
+    if [ -f $Files ]; then
+        source $Files;
+        cat $Files;
+    fi
+}
+
+# Extend buildin cd
+cd () {
+    builtin cd "$@";
+    load_local_bash;
+}
