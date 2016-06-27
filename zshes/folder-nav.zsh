8a62c7c6796aff706e8c94c96e485af0c89cd151
Rene Merino <rmerino@amayamedia.com>
2016-06-27 14:52:28 -0400

    Added simulator alias

--- zshes/folder-nav.zsh
+++ zshes/folder-nav.zsh
@@ -3,6 +3,10 @@ alias conf='cd ~/DotFiles'
 alias home='$HOME'
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
 
+# Open iOS Simulator if is installed
+# Works with xCode 7.*
+alias simulator='open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
+
 # mkdir and cd into that dir
 mkd() {
     mkdir -p $1
