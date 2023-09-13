d61d8469a612b03a9912c6e638ecfabbc719baf1
Rene Merino <rene@happycog.com>
2023-09-13 14:07:46 -0400

    Update shell.sh

--- install/shell.sh
+++ install/shell.sh
@@ -1,4 +1,4 @@
 echo "Setting symlinks"
 sh symlinks.sh
-echo "Adding zsh syntax highlighting"
-($dry cd ~/.oh-my-zsh/custom/plugins && $dry git clone git://github.com/zsh-users/zsh-syntax-highlighting.git)
+# echo "Adding zsh syntax highlighting"
+# ($dry cd ~/.oh-my-zsh/custom/plugins && $dry git clone git://github.com/zsh-users/zsh-syntax-highlighting.git)
