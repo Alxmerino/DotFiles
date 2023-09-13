acb176fa7ff64d87a2e68b19356061ea5ca331fb
Rene Merino <rene@happycog.com>
2023-09-13 14:10:49 -0400

    Update apps.sh

--- install/apps.sh
+++ install/apps.sh
@@ -2,6 +2,9 @@
 $dry brew install caskroom/cask/brew-cask
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # install all the apps
 apps=$(<data/apps.txt)
 echo "Installing all the apps"
